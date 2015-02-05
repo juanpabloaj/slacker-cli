caafd423913626d7313d442d3370f171d14f5c90
JuanPablo <jpabloaj@gmail.com>
2015-02-04 23:40:52 -0300

    bump to 0.1.0
    * send message to channel
    * send message to user
    * upload file to channel

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(*paths):
 
 setup(
     name="slacker-cli",
-    version="0.0.5",
+    version="0.1.0",
     description="Send messages to slack from command line",
     long_description=(read('README.rst')),
     url="https://github.com/juanpabloaj/slacker-cli",
