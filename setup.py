0e6ef907bc177010dbab7ecb55183693b24ac623
JuanPablo <jpabloaj@gmail.com>
2015-03-28 23:57:36 -0300

    bump to 0.1.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*paths):
 
 setup(
     name="slacker-cli",
-    version="0.1.2",
+    version="0.1.3",
     description="Send messages to slack from command line",
     long_description=(read('README.rst')),
     url="https://github.com/juanpabloaj/slacker-cli",
