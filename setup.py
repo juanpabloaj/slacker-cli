85e45c9e4cd700d62b0a0276b7e818c5e948b3ce
JuanPablo <jpabloaj@gmail.com>
2017-02-08 10:16:55 -0300

    keep python 2.6

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
