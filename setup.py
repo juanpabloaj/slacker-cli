d73c597b3e200cc6524631b896887b852d5dae80
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-01-12 12:28:06 +0200

    Add support for Python 3.11, test 3.12-dev, drop EOL 3.6

--- setup.py
+++ setup.py
@@ -27,14 +27,14 @@ setup(
     packages=["slacker_cli"],
     test_suite="tests",
     entry_points={"console_scripts": ["slacker=slacker_cli:main"]},
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
